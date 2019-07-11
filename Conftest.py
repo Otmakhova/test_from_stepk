a27d293f80a9a38560deee547af85f0c575fc144
uotmakhova <mi@DESKTOP-E5SR6IP>
2019-07-11 15:46:18 +0300

    project initialization

---
+++ Conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
+    browser = webdriver.Chrome(executable_path='C:/chromedriver/chromedriver.exe',options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
