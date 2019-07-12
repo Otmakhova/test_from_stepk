4c3a860f3424a956a568695da928b3a30443c6dd
uotmakhova <mi@DESKTOP-E5SR6IP>
2019-07-12 15:54:45 +0300

    move login to base page

--- test_product_page.py
+++ test_product_page.py
@@ -18,6 +18,20 @@ def test_guest_can_add_product_to_cart(browser, link):
     page.add_product_to_basket()
 
 @pytest.mark.run
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    
+@pytest.mark.run
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 def test_guest_cant_see_success_message(browser): 
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
